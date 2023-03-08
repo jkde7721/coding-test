73a68332fc740521bdc2c21e453225c41d32c3ed
jkde7721 <jkde7721@naver.com>
2023-03-08 14:59:59 +0900

    day35 solved

--- 3_완전탐색,백트래킹/12851.cpp
+++ 3_완전탐색,백트래킹/12851.cpp
@@ -1,3 +1,9 @@
+/**
+ * 재귀 풀이
+ * 메모리: 3324KB
+ * 시간: 1436ms
+*/
+/*
 #include <iostream>
 using namespace std;
 
@@ -27,6 +33,54 @@ int main() {
 
     cin >> N >> K;
     find(N, 0);
+    cout << ret << '\n';
+    cout << num << '\n';
+
+    return 0;
+}
+*/
+
+/**
+ * BFS 풀이
+ * 메모리: 2940KB
+ * 시간: 0ms
+*/
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N, K, n, cnt, ret = 100000, num, visited[100001], d[3] = {2, 1, -1};
+queue<pair<int, int>> q;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> K;
+    q.push({N, 0});
+    while (q.size()) {
+        n = q.front().first; cnt = q.front().second;
+        q.pop();
+
+        if (n == K) {
+            if (ret == cnt) num++;
+            else if (ret > cnt) num = 1;
+            ret = min(ret, cnt);
+            continue;
+        }
+
+        if (cnt + 1 > ret) continue;
+        for (int i = 0; i < 3; i++) {
+            int nn = (i == 0) ? n * d[i] : n + d[i];
+            if (nn < 0 || nn > 100000) continue;
+            if (visited[nn] > 0 && visited[nn] < cnt + 1) continue;
+
+            visited[nn] = cnt + 1;
+            q.push({nn, cnt + 1});
+        }
+    }
+
     cout << ret << '\n';
     cout << num << '\n';
 
