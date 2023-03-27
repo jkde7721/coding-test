4ccff8eb8537a8810bc44e2d416e19e2fbe3986c
jkde7721 <jkde7721@naver.com>
2023-03-27 15:35:29 +0900

    2529 다른 풀이

--- 3_완전탐색,백트래킹/2529.cpp
+++ 3_완전탐색,백트래킹/2529.cpp
@@ -7,14 +7,9 @@ char sign[9];
 string min_ret = "9876543210", max_ret = "0", anw;
 bool num[10];
 
-string getMin(string a, string b) {
-    if (stoll(a) < stoll(b)) return a;
-    else return b;
-}
-
-string getMax(string a, string b) {
-    if (stoll(a) > stoll(b)) return a;
-    else return b;
+bool isGt(string a, string b) {
+    if (a.size() == b.size()) return a > b;
+    return a.size() > b.size();
 }
 
 bool isOk(int a, int b, char op) {
@@ -24,8 +19,8 @@ bool isOk(int a, int b, char op) {
 
 void dfs(int dep) {
     if (dep == k + 1) {
-        min_ret = getMin(min_ret, anw);
-        max_ret = getMax(max_ret, anw);
+        min_ret = isGt(min_ret, anw) ? anw : min_ret;
+        max_ret = isGt(max_ret, anw) ? max_ret : anw;
         return;
     }
 
@@ -54,3 +49,58 @@ int main() {
 
     return 0;
 }
+
+//메모리: 3692KB, 시간: 12ms 
+//→ 생성되는 모든 숫자 문자열을 vector에 저장하고 후에 정렬하기 때문에 메모리 더 씀, 시간 더 걸림
+/*
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int k;
+char sign[9];
+bool nums[10];
+vector<string> ret;
+
+bool isOk(char a, char b, char s) {
+    if (s == '<') return a < b;
+    return a > b;
+}
+
+void go(int idx, string anw) {
+    if (idx == k + 1) {
+        ret.push_back(anw);
+        return;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (nums[i]) continue;
+        if (idx > 0 && !isOk(anw[anw.size() - 1], i + '0', sign[idx - 1])) continue;
+        
+        nums[i] = 1;
+        go(idx + 1, anw + char(i + '0'));
+        nums[i] = 0;
+    }
+}
+
+bool cmp(string a, string b) {
+    if (a.size() == b.size()) return a < b;
+    return a.size() < b.size();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> k;
+    for (int i = 0; i < k; i++) {
+        cin >> sign[i];
+    }
+    go(0, "");
+
+    sort(ret.begin(), ret.end(), cmp);
+    cout << ret[ret.size() - 1] << '\n' << ret[0] << '\n';
+
+    return 0; 
+}
+*/
