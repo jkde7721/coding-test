0f9bbc80a414987f196a1c7b0c78fb4e87985e74
jkde7721 <jkde7721@naver.com>
2023-04-02 12:12:10 +0900

    1062 다른 풀이

--- 4_비트마스킹/1062.cpp
+++ 4_비트마스킹/1062.cpp
@@ -1,4 +1,5 @@
 //메모리: 2024KB, 시간: 36ms
+/*
 #include <iostream>
 using namespace std;
 
@@ -43,6 +44,44 @@ int main() {
         go(0, 0);
         cout << ret << '\n';
     }
+
+    return 0;
+}
+*/
+
+//메모리: 2024KB, 시간: 128ms
+//for문 + 재귀 보다 재귀 + 재귀가 조금 더 걸림
+#include <iostream>
+using namespace std;
+
+int n, k, word[50];
+string str;
+
+int go(int idx, int cnt, int mask) {
+    if (cnt == k + 1) return 0;
+    if (idx == 26) {
+        int tmp = 0;
+        for (int i = 0; i < n; i++) {
+            if ((word[i] & mask) == word[i]) tmp++;
+        }
+        return tmp;
+    }
+    int ret = go(idx + 1, cnt + 1, mask | (1 << idx));
+    if (idx != 'a' - 'a' && idx != 'c' - 'a' && idx != 'i' - 'a' && idx != 'n' - 'a' && idx != 't' - 'a') {
+        ret = max(ret, go(idx + 1, cnt, mask)); 
+    }
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        for (char c : str) word[i] |= 1 << (c - 'a');
+    }
+    cout << go(0, 0, 0) << '\n';
 
     return 0;
 }
