a0150b5d52c1b58acda2ef91f0c7e93cc1742be5
jkde7721 <jkde7721@naver.com>
2023-04-12 15:55:17 +0900

    2109 다른 풀이

--- 5_그리디,라인스위핑,투포인터/2109.cpp
+++ 5_그리디,라인스위핑,투포인터/2109.cpp
@@ -31,5 +31,32 @@ int main() {
 }
 */
 
-//메모리: KB, 시간: ms
+//메모리: 2292KB, 시간: 4ms
 //작은 값이 우선순위가 더 높은 큐 이용해서 풀어보기
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int n, p, d, ret;
+vector<pair<int, int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> p >> d;
+        v.push_back({d, p});
+    }
+    sort(v.begin(), v.end());
+    for (auto it : v) {
+        pq.push(it.second);
+        if (pq.size() > it.first) pq.pop();
+    }
+    while (pq.size()) { ret += pq.top(); pq.pop(); }
+    cout << ret << '\n';
+
+    return 0;
+}
