54d07ac308c796644b6886b02d30cf8d2f4c8111
jkde7721 <jkde7721@naver.com>
2023-04-13 13:30:41 +0900

    1931 다른 풀이

--- 5_그리디,라인스위핑,투포인터/1931.cpp
+++ 5_그리디,라인스위핑,투포인터/1931.cpp
@@ -1,4 +1,5 @@
 //메모리: 3688KB, 시간: 28ms
+/*
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -37,6 +38,36 @@ int main() {
 
     return 0;
 }
+*/
 
-//메모리: KB, 시간: ms
+//메모리: 3688KB, 시간: 28ms
 //종료 시간 기준으로만 정렬해서 풀어보기
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, s, e, ret = 1;
+vector<pair<int, int>> v;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s >> e;
+        v.push_back({e, s});
+    }
+    sort(v.begin(), v.end());
+
+    s = v[0].second; e = v[0].first;
+    for (int i = 1; i < n; i++) {
+        if (v[i].second >= e) {
+            s = v[i].second; e = v[i].first;
+            ret++;
+        }
+    }
+    cout << ret << '\n';
+
+    return 0;
+}
