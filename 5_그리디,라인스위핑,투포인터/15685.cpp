b67a83a9cf40ff19c6d528ef5b51e538414113e1
jkde7721 <jkde7721@naver.com>
2023-04-24 11:45:35 +0900

    15685 다른 풀이

--- 5_그리디,라인스위핑,투포인터/15685.cpp
+++ 5_그리디,라인스위핑,투포인터/15685.cpp
@@ -1,4 +1,5 @@
 //메모리: 2168KB, 시간: 0ms
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -54,7 +55,102 @@ int main() {
 
     return 0;
 }
+*/
 
-//메모리: KB, 시간: ms
-//과거 세대 드래곤 커브에서 중요한 것은 y, x 인덱스 자체가 아닌 진행 방향 
+//메모리: 2032KB, 시간: 0ms
+//과거 세대 드래곤 커브에서 중요한 것은 y, x 좌표 자체가 아닌 진행 방향 
 //→ vector에 pair<int, int>가 아닌 int 자료형만 사용해서 다시 풀어보기
+#include <bits/stdc++.h>
+using namespace std;
+
+const int dy[] = {0, -1, 0, 1};
+const int dx[] = {1, 0, -1, 0};
+int N, x, y, d, g, ret;
+bool _map[101][101];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> x >> y >> d >> g;
+        vector<int> v;
+        _map[y][x] = _map[y+dy[d]][x+dx[d]] = 1;
+        y += dy[d]; x += dx[d];
+        v.push_back(d);
+        while (g > 0) {
+            for (int j = v.size() - 1; j >= 0; j--) {
+                d = (v[j] + 1) % 4;
+                y += dy[d]; x += dx[d];
+                v.push_back(d);
+                _map[y][x] = 1;
+            }
+            g--;
+        }
+    }
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (_map[i][j] && _map[i+1][j] && _map[i][j+1] && _map[i+1][j+1]) ret++;
+        }
+    }
+    cout << ret << '\n';
+
+    return 0;
+}
+
+//메모리: 2164KB, 시간: 0ms
+/*
+드래곤 커브에서 세대가 모두 같을 때 서로 다른 커브의 양상은 4개 밖에 없음
+→즉 시작 방향에 따라 커브가 진행되는 양상이 달라짐, 이때 시작 방향은 상하좌우로 only 4개
+→커브의 진행 양상을 vector로 관리할 때 시작 점이 주어지면 전체 커브의 좌표를 구할 수 있음
+*/
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+const int dy[] = {0, -1, 0, 1};
+const int dx[] = {1, 0, -1, 0};
+int N, ret, x, y, d, g;
+bool _map[101][101];
+vector<int> v[4][11];
+
+void makeDragon() {
+    for (int i = 0; i < 4; i++) {
+        v[i][0].push_back(i);
+        v[i][1].push_back((i+1)%4);
+        for (int j = 2; j <= 10; j++) {
+            for (int k = v[i][j-1].size() - 1; k >= 0; k--) {
+                v[i][j].push_back((v[i][j-1][k]+1)%4);
+            }
+            for (int k = 0; k < v[i][j-1].size(); k++) {
+                v[i][j].push_back(v[i][j-1][k]);
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N;
+    makeDragon(); //시작 방향에 따른 4개의 전체 세대 드래곤 커브 생성
+    for (int i = 0; i < N; i++) {
+        cin >> x >> y >> d >> g;
+        _map[y][x] = 1;
+        for (int j = 0; j <= g; j++) {
+            for (int dir : v[d][j]) {
+                y += dy[dir]; x += dx[dir];
+                _map[y][x] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (_map[i][j] && _map[i+1][j] && _map[i][j+1] && _map[i+1][j+1]) ret++;
+        }
+    }
+    cout << ret << '\n';
+
+    return 0;
+}
+*/
