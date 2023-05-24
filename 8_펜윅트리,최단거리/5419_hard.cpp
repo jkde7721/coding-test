32c7b964c3927afa442d46c1bb354d9ef9af6a9c
jkde7721 <jkde7721@naver.com>
2023-05-24 20:03:05 +0900

    day102 solved

---
+++ 8_펜윅트리,최단거리/5419_hard.cpp
@@ -0,0 +1,62 @@
+//메모리: 3544KB, 시간: 344ms
+/*
+- 현재 자기보다 x값이 작거나 같으면서 y값은 크거나 같은 좌표의 개수를 count하여 ret에 플러스
+- 현재 x까지의 각 y좌표에 있는 섬의 개수를 tree에 저장하고 동적으로 업데이트
+- 현재 자기보다 x값이 작거나 같으면서 y값은 크거나 같은 좌표의 개수는 tree 값의 누적합
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int t, n, idx;
+ll ret;
+pair<int, int> land[75000];
+vector<int> _y;
+
+ll sum(vector<int>& tree, int node) {
+    ll ret = 0;
+    while (node > 0) {
+        ret += tree[node];
+        node -= node & -node;
+    }
+    return ret;
+}
+
+void update(vector<int>& tree, int node, int diff) {
+    while (node < tree.size()) {
+        tree[node] += diff;
+        node += node & -node;
+    }
+}
+
+bool cmp(pair<int, int> a, pair<int, int> b) {
+    if (a.first == b.first) return a.second > b.second;
+    return a.first < b.first;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> t;
+    while (t--) {
+        cin >> n; _y.clear(); ret = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> land[i].first >> land[i].second;
+            _y.push_back(land[i].second * -1);
+        }
+        sort(land, land + n, cmp);
+        sort(_y.begin(), _y.end());
+        _y.erase(unique(_y.begin(), _y.end()), _y.end());
+        vector<int> tree(_y.size() + 1); //현재 x값까지의 해당 y값 축에 있는 섬 좌표 개수 저장
+
+        idx = (lower_bound(_y.begin(), _y.end(), land[0].second * -1) - _y.begin()) + 1;
+        update(tree, idx, 1);
+        for (int i = 1; i < n; i++) {
+            idx = (lower_bound(_y.begin(), _y.end(), land[i].second * -1) - _y.begin()) + 1;
+            ret += sum(tree, idx); update(tree, idx, 1);
+        }
+        cout << ret << '\n';
+    }
+
+    return 0;
+}
